be8a1c25c983bac2abcf1790d6faec5192e82473
god-of-ghz <god-of-ghz@users.noreply.github.com>
2026-04-28 23:33:59 -0400

    chore: remove scratch files from tracking and update .gitignore

--- server/scratch.ts
+++
@@ -1,7 +0,0 @@
-import sqlite3 from 'sqlite3';
-
-const db = new sqlite3.Database('./data/node.db');
-db.all("SELECT name FROM sqlite_master WHERE type='table'", (err, rows) => {
-    if (err) console.error(err);
-    else console.log("TABLES:", rows);
-});
